6c3c1f545581b947b5b0cb57aecfaf2f03e59c4b
Ellis Hampton <hamptonelkav@gmail.com>
2025-04-09 11:08:35 -0700

    Add basic database classes

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, render_template, abort
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    firstname = db.Column(db.String(80), nullable=False)
+    lastname = db.Column(db.String(80), nullable=False)
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class Teacher(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    firstname = db.Column(db.String(80), nullable=False)
+    lastname = db.Column(db.String(80), nullable=False)
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class Class(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    course_name = db.Column(db.String(100), unique=True, nullable=False)
+    teacher = db.Column(db.Integer, db.foreignKey('user.id'))
+    time = db.Column(db.String(80))
+    students_enrolled = db.Column(db.Integer, nullable=False)
+    max_students = db.Column(db.Integer, nullable=False)
+    def __repr__(self):
+        return '<Class %r>' % self.course_name
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(port=5000)
